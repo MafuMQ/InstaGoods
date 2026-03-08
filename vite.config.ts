571c8b79f9d7993993b6285c3d52f2b4751b59c1
Bongani Masanabo <masanabodb@gmail.com>
2026-03-08 22:34:13 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -32,6 +32,7 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: ['react', 'react-dom'],
   },
   build: {
     // Enable rollup optimizations
